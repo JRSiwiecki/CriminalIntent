96d0c7c577595972398e9accb2b7a30a8dee608e
JRSiwiecki <JRSiwiecki@gmail.com>
2023-09-28 18:46:11 -0700

    Refactor CrimeDetailFragment, create CrimeDetailViewModel

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeDetailFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeDetailFragment.kt
@@ -12,8 +12,6 @@ import com.bignerdranch.android.criminalintent.databinding.FragmentCrimeDetailBi
 import java.util.UUID
 import java.util.Date
 
-private const val TAG = "CrimeDetailFragment"
-
 class CrimeDetailFragment: Fragment() {
 
     private var _binding: FragmentCrimeDetailBinding? = null
@@ -22,22 +20,7 @@ class CrimeDetailFragment: Fragment() {
             "Cannot access binding because it is null. Is the view visible?"
         }
 
-    private lateinit var crime: Crime
-
     private val args: CrimeDetailFragmentArgs by navArgs()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        crime = Crime(
-            id = UUID.randomUUID(),
-            title = "",
-            date = Date(),
-            isSolved = false
-        )
-
-        Log.d(TAG, "The crime ID is: ${args.crimeId}")
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -53,16 +36,13 @@ class CrimeDetailFragment: Fragment() {
 
         binding.apply {
             crimeTitle.doOnTextChanged { text, _, _, _ ->
-                crime = crime.copy(title = text.toString())
             }
 
             crimeDate.apply {
-                text = crime.date.toString()
                 isEnabled = false
             }
 
             crimeSolved.setOnCheckedChangeListener { _, isChecked ->
-                crime = crime.copy(isSolved = isChecked)
             }
         }
     }
