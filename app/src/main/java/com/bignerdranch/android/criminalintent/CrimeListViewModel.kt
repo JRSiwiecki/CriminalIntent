77da1837fdba487bb75543e7103da009b5621007
Joseph <46443751+JRSiwiecki@users.noreply.github.com>
2023-09-30 13:04:39 -0700

    Finish Chapter 15 (#7)
    * Add new assets
    
    * Add ADD button to app bar
    
    * Allow user to add crimes, remove dummy data

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
@@ -25,5 +25,9 @@ class CrimeListViewModel: ViewModel() {
                 }
             }
         }
+
+    suspend fun addCrime(crime: Crime) {
+        crimeRepository.addCrime(crime)
+    }
 }
 
--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeRepository.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeRepository.kt
@@ -19,7 +19,6 @@ class CrimeRepository private constructor(context: Context,
             CrimeDatabase::class.java,
             DATABASE_NAME
         )
-        .createFromAsset(DATABASE_NAME)
         .build()
 
     fun getCrimes(): Flow<List<Crime>> = database.crimeDao().getCrimes()
@@ -28,6 +27,10 @@ class CrimeRepository private constructor(context: Context,
 
     fun updateCrime(crime: Crime) {
         coroutineScope.launch { database.crimeDao().updateCrime(crime) }
+    }
+
+    suspend fun addCrime(crime: Crime) {
+        database.crimeDao().addCrime(crime)
     }
 
     companion object {
